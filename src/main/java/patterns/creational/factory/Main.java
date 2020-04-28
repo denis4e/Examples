1d29cdf57e071ab4f4888563cfc9511af89d4d84
dsosuliev <dsosuliev@ascendix.net>
2020-04-29 02:06:31 +0300

    patterns: refactoring

--- src/main/java/patterns/creational/factory/Main.java
+++ src/main/java/patterns/creational/factory/Main.java
@@ -2,44 +2,100 @@ package patterns.creational.factory;
 
 public class Main {
     public static void main(String[] args) {
-        CarFactory carFactory = new CarFactory();
-        Car toyota = carFactory.create("Toyota");
-        toyota.drive();
-        Car audi = carFactory.create("Audi");
-        audi.drive();
+        TeamFactory apiTeam = new FactoryMaker().createFactory("API");
+        DevMember devAPI = apiTeam.createDevMember();
+        QaMember qaAPI = apiTeam.createQaMember();
+        devAPI.writeCode();
+        qaAPI.doTests();
+        TeamFactory webTeam = new FactoryMaker().createFactory("WEB");
+        QaMember qaWEB = webTeam.createQaMember();
+        DevMember devWEB = webTeam.createDevMember();
+        qaWEB.doTests();
+        devWEB.writeCode();
     }
 }
 
-interface Car {
-    void drive();
+interface TeamFactory {
+    QaMember createQaMember();
+
+    DevMember createDevMember();
 }
 
-class Toyota implements Car {
+interface QaMember {
+    void doTests();
+}
+
+interface DevMember {
+    void writeCode();
+}
+
+
+class ManualQa implements QaMember {
 
     @Override
-    public void drive() {
-        System.out.println("drive Toyota");
+    public void doTests() {
+        System.out.println("Start manual tests");
     }
 }
 
-class Audi implements Car {
+class AutomationQa implements QaMember {
 
     @Override
-    public void drive() {
-        System.out.println("drive Audi");
+    public void doTests() {
+        System.out.println("Start auto tests");
     }
 }
 
-class CarFactory {
-    public Car create(String type) {
-        if (type == null) {
-            return null;
-        }
-        if (type.equalsIgnoreCase("TOYOTA")) {
-            return new Toyota();
+class FrontEndDev implements DevMember {
 
-        } else if (type.equalsIgnoreCase("AUDI")) {
-            return new Audi();
+    @Override
+    public void writeCode() {
+        System.out.println("write frontend");
+    }
+}
+
+class BackEndDev implements DevMember {
+
+    @Override
+    public void writeCode() {
+        System.out.println("write backend");
+    }
+}
+
+
+class WebProjectFactory implements TeamFactory {
+    @Override
+    public QaMember createQaMember() {
+        return new AutomationQa();
+    }
+
+    @Override
+    public DevMember createDevMember() {
+        return new FrontEndDev();
+    }
+}
+
+
+class APIProjectFactory implements TeamFactory {
+    @Override
+    public QaMember createQaMember() {
+        return new ManualQa();
+    }
+
+    @Override
+    public DevMember createDevMember() {
+        return new BackEndDev();
+    }
+
+}
+
+class FactoryMaker {
+    public TeamFactory createFactory(String factoryType) {
+        if (factoryType.equalsIgnoreCase("API")) {
+            return new APIProjectFactory();
+
+        } else if (factoryType.equalsIgnoreCase("WEB")) {
+            return new WebProjectFactory();
 
         }
         return null;
